8f46201a9618864014ef39c7005f6a6dd0bade01
DANIEL CIUCUR <ciucur.daniel14@gmail.com>
2023-11-06 19:43:49 +0200

    Add more relevant outputs for the module

--- aks/outputs.tf
+++ aks/outputs.tf
@@ -1,9 +1,55 @@
+output "aks_id" {
+  description = "The `azurerm_kubernetes_cluster`'s id."
+  value       = azurerm_kubernetes_cluster.main.id
+}
+
+output "aks_name" {
+  description = "The `aurerm_kubernetes-cluster`'s name."
+  value       = azurerm_kubernetes_cluster.main.name
+}
+
+output "host" {
+  description = "The `host` in the `azurerm_kubernetes_cluster`'s `kube_config` block. The Kubernetes cluster server host."
+  sensitive   = true
+  value       = azurerm_kubernetes_cluster.main.kube_config[0].host
+}
+
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.main.kube_config.0.client_certificate
-  sensitive = true
+  description = "The `client_certificate` in the `azurerm_kubernetes_cluster`'s `kube_config` block. Base64 encoded public certificate used by clients to authenticate to the Kubernetes cluster."
+  value       = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate
+  sensitive   = true
+}
+
+output "client_key" {
+  description = "The `client_key` in the `azurerm_kubernetes_cluster`'s `kube_config` block. Base64 encoded private key used by clients to authenticate to the Kubernetes cluster."
+  sensitive   = true
+  value       = azurerm_kubernetes_cluster.main.kube_config[0].client_key
+}
+
+output "cluster_ca_certificate" {
+  description = "The `cluster_ca_certificate` in the `azurerm_kubernetes_cluster`'s `kube_config` block. Base64 encoded public CA certificate used as the root of trust for the Kubernetes cluster."
+  sensitive   = true
+  value       = azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
+}
+
+output "password" {
+  description = "The `password` in the `azurerm_kubernetes_cluster`'s `kube_config` block. A password or token used to authenticate to the Kubernetes cluster."
+  sensitive   = true
+  value       = azurerm_kubernetes_cluster.main.kube_config[0].password
+}
+
+output "username" {
+  description = "The `username` in the `azurerm_kubernetes_cluster`'s `kube_config` block. A username used to authenticate to the Kubernetes cluster."
+  sensitive   = true
+  value       = azurerm_kubernetes_cluster.main.kube_config[0].username
 }
 
 output "kube_config_raw" {
   value     = azurerm_kubernetes_cluster.main.kube_config_raw
   sensitive = true
+}
+
+output "oidc_issuer_url" {
+  description = "The OIDC issuer URL that is associated with the cluster."
+  value       = azurerm_kubernetes_cluster.main.oidc_issuer_url
 }
